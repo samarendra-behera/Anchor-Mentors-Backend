5105904b838c0b38ba6e85a426c999dadbbf13cc
samarendrabehera.papu@gmail.com <samarendrabehera.papu@gmail.com>
2024-06-30 23:01:44 +0530

    update

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const express = require('express')
 
+const authRouter = require('./route/authRoute')
+
 const app = express()
 
 
 app.get('/', (req, res)=> {
     res.status(200).json({
         status: 'success',
-        message: 'Woohoo! REST APIs are working',
+        message: 'Wahoo! REST APIs are working',
     });
 });
 
+
+// all routes will be here
+app.use('/api/v1/auth', authRouter)
 
 app.listen(5000, ()=> {
     console.log('Server up and running')
---
+++ route/authRoute.js
@@ -0,0 +1,8 @@
+const { signup } = require('../controller/authController')
+
+const router = require('express').Router();
+
+router.route('/signup').post(signup);
+
+
+module.exports = router;
