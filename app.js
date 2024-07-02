e0357089182435333e63a9ae1a5349c514ac037c
@samarendrabehera.papu <samarendrabehera.papu@gmail.com>
2024-07-02 20:00:36 +0530

    update

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 require('dotenv').config({ path: `${process.cwd()}/.env` })
 const express = require('express')
 
+const catchAsync = require('./utils/catchAsync')
+const AppError = require('./utils/appError')
+
 const authRouter = require('./route/authRoute')
 
 const app = express()
@@ -20,13 +23,22 @@ app.get('/', (req, res)=> {
 app.use('/api/v1/auth', authRouter)
 
 
-app.use('*', (req, res)=> {
+app.use('*', catchAsync(async(req, res, next )=> {
+    throw new AppError('This an error ', 400);
     res.status(404).json({
         status: 'fail',
         message: 'Route not found'
+    });
+}));
+
+app.use((err, req, res, next)=> {
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+        stack: err.stack
     });
 });
 
 app.listen(process.env.APP_PORT, ()=> {
     console.log('Server up and running on port', process.env.APP_PORT);
-})
+});
