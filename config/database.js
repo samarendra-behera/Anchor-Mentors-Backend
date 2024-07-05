5613702cd8b0d7e2944196e610c78b339e0838b7
@samarendrabehera.papu <samarendrabehera.papu@gmail.com>
2024-07-05 19:08:50 +0530

    update

--- config/database.js
+++ config/database.js
@@ -1,5 +1,9 @@
-const {Sequelize}  = require('sequelize');
+const { Sequelize } = require('sequelize');
 
+require('pg').types.setTypeParser(1114, stringValue => {
+    return new Date(stringValue + '+05:30');
+    // e.g., UTC offset. Use any offset that you would like.
+});
 
 const env = process.env.NODE_ENV || 'development';
 const config = require('../config/config.js')[env];
